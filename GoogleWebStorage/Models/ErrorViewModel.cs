c567c6a8245a7ad08e420d056a427e266fbb8f2c
yssavaliya <ysavaliya215@rku.ac.in>
2024-07-06 16:39:08 +0530

    first commit

---
+++ GoogleWebStorage/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace GoogleWebStorage.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
